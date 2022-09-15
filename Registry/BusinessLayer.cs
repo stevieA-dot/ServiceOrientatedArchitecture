371cb14f0e1b87dd186ad9e6fc461c05c8c1c34a
Stevie Hollard <20236092@student.curtin.edu.au>
2022-09-15 11:10:21 +0800

    Added the registry project

---
+++ Registry/BusinessLayer.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+using Authenticator;
+
+using CsvHelper;
+
+using static APIClasses.Registry;
+
+namespace Registry
+{
+    public class BusinessLayer
+    {
+        private const string _fileName = "apiEndpoints.txt";
+
+        public static bool Authenticate(int token)
+        {
+            NetTcpBinding tcp = new NetTcpBinding();
+            string url = "net.tcp://localhost:8100/Authenticator";
+            ChannelFactory<IAuthenticator> authServerFactory = new ChannelFactory<IAuthenticator>(tcp, url);
+            IAuthenticator authServer = authServerFactory.CreateChannel();
+
+            return authServer.Validate(token);
+        }
+        public static void WriteEndPointToFile(EndpointData data)
+        {
+            if (!File.Exists(_fileName))
+            {
+                using (var writer = new StreamWriter(_fileName))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.Context.RegisterClassMap<EndpointDataMap>();
+                    csv.WriteHeader<EndpointData>();
+                    csv.NextRecord();
+                    csv.WriteRecord(data);
+                }
+            }
+            else
+            {
+                using (var writer = new StreamWriter(_fileName, true))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.Context.RegisterClassMap<EndpointDataMap>();
+                    csv.NextRecord();
+                    csv.WriteRecord(data);
+                }
+            }
+        }
+
+        public static List<EndpointData> ReturnAllServices()
+        {
+            List<EndpointData> endpoints = new List<EndpointData>();
+            if (File.Exists(_fileName))
+            {
+                using (var reader = new StreamReader(_fileName))
+                using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+                {
+                    csv.Context.RegisterClassMap<EndpointDataMap>();
+                    endpoints = csv.GetRecords<EndpointData>().ToList();
+                }
+            }
+
+            return endpoints;
+        }
+
+        public static void RemoveEndPoint(EndpointData endpoint)
+        {
+            var endpoints = ReturnAllServices();
+
+            if (endpoints.Count != 0)
+            {
+                var foundEndpoint = endpoints.Where(e => e.Name == endpoint.Name).FirstOrDefault();
+
+                if (foundEndpoint != null)
+                {
+                    endpoints.Remove(foundEndpoint);
+
+                    using (var writer = new StreamWriter(_fileName))
+                    using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        csv.Context.RegisterClassMap<EndpointDataMap>();
+                        csv.WriteHeader<EndpointData>();
+                        csv.NextRecord();
+                        csv.WriteRecords(endpoints);
+                    }
+                }
+            }
+        }
+
+        public static List<EndpointData> FindEndpointWithSearchTerm(SearchData data)
+        {
+            var endpoints = ReturnAllServices();
+            List<EndpointData> result = new List<EndpointData>();
+
+            if (endpoints.Count != 0)
+            {
+                result = endpoints.Where(e => e.APIEndpoint.Contains(data.SearchStr)).ToList();
+            }
+
+            return result;
+        }
+
+
+    }
+}
---
+++ Registry/Controllers/ValuesController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+using static APIClasses.Registry;
+
+namespace Registry.Controllers
+{
+    [RoutePrefix("api/Registry")]
+    public class ValuesController : ApiController
+    {
+        private const string _status = "Denied";
+        private const string _reason = "Authentication Error";
+
+        [Route("publish/{token}/{endpointData}")]
+        public DataTable Publish(int token, [FromBody] EndpointData endpointData)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Status");
+            dt.Columns.Add("Reason");
+
+            if (BusinessLayer.Authenticate(token))
+            {
+                BusinessLayer.WriteEndPointToFile(endpointData);
+            }
+            else
+            {
+                dt.Rows.Add(_status, _reason);
+            }
+
+            return dt;
+        }
+
+        [Route("search/{token}/{searchData}")]
+        public DataTable Search(int token, [FromBody] SearchData searchData)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Status");
+            dt.Columns.Add("Reason");
+
+            if (BusinessLayer.Authenticate(token))
+            {
+                List<EndpointData> endpoints = BusinessLayer.FindEndpointWithSearchTerm(searchData);
+
+                if (endpoints.Count() != 0)
+                {
+                    dt.Columns.Add("Name");
+                    dt.Columns.Add("Description");
+                    dt.Columns.Add("APIEndpoint");
+                    dt.Columns.Add("NumOfOperands");
+                    dt.Columns.Add("OperandType");
+
+                    foreach (var endpoint in endpoints)
+                    {
+                        dt.Rows.Add(
+                            endpoint.Name,
+                            endpoint.Description,
+                            endpoint.APIEndpoint,
+                            endpoint.NumOfOperands,
+                            endpoint.OperandType);
+                    }
+                }
+                
+            }
+            else
+            {
+                dt.Rows.Add(_status, _reason);
+            }
+
+            return dt;
+        }
+
+        [Route("allServices/{token}")]
+        public DataTable AllServices(int token)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Status");
+            dt.Columns.Add("Reason");
+
+            if (BusinessLayer.Authenticate(token))
+            {
+                List<EndpointData> endpoints = BusinessLayer.ReturnAllServices();
+
+                if (endpoints.Count() != 0)
+                {
+                    dt.Columns.Add("Name");
+                    dt.Columns.Add("Description");
+                    dt.Columns.Add("APIEndpoint");
+                    dt.Columns.Add("NumOfOperands");
+                    dt.Columns.Add("OperandType");
+
+                    foreach (var endpoint in endpoints)
+                    {
+                        dt.Rows.Add(
+                            endpoint.Name,
+                            endpoint.Description,
+                            endpoint.APIEndpoint,
+                            endpoint.NumOfOperands,
+                            endpoint.OperandType);
+                    }
+                }
+                
+            }
+            else
+            {
+                dt.Rows.Add(_status, _reason);
+            }
+
+            return dt;
+        }
+
+        [Route("unpublish/{token}/{endpoint}")]
+        public DataTable Unpublish(int token, [FromBody] EndpointData endpoint)
+        {
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Status");
+            dt.Columns.Add("Reason");
+
+            if (BusinessLayer.Authenticate(token))
+            {
+                BusinessLayer.RemoveEndPoint(endpoint);
+            }
+            else
+            {
+                dt.Rows.Add(_status, _reason);
+            }
+
+            return dt;
+        }
+    }
+}
