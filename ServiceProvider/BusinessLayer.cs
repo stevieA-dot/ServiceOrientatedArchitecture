9085a77cb230633a2e00457a7630f596a054f842
Stevie Hollard <20236092@student.curtin.edu.au>
2022-09-15 10:25:45 +0800

    Addition of business layer service provider API.

---
+++ ServiceProvider/BusinessLayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Web;
+
+using Authenticator;
+
+namespace ServiceProvider
+{
+    public class BusinessLayer
+    {
+        public static bool Authenticate(int token)
+        {
+            NetTcpBinding tcp = new NetTcpBinding();
+            string url = "net.tcp://localhost:8100/Authenticator";
+            ChannelFactory<IAuthenticator> authServerFactory = new ChannelFactory<IAuthenticator>(tcp, url);
+            IAuthenticator authServer = authServerFactory.CreateChannel();
+            
+            return authServer.Validate(token);
+        }
+    }
+}
