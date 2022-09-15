772c6c7a860156094cb25ca739c2a177c0824236
Stevie Hollard <20236092@student.curtin.edu.au>
2022-09-15 11:10:49 +0800

    Updated the API classes to include the registry project classes

---
+++ APIClasses/Registry.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CsvHelper.Configuration;
+
+namespace APIClasses
+{
+    public class Registry
+    {
+        public class EndpointData
+        {
+            public string Name;
+            public string Description;
+            public string APIEndpoint;
+            public string NumOfOperands;
+            public string OperandType;
+        }
+
+        public class SearchData
+        {
+            public string SearchStr;
+        }
+
+        public sealed class EndpointDataMap : ClassMap<EndpointData>
+        {
+            public EndpointDataMap()
+            {
+                Map(m => m.Name);
+                Map(m => m.Description);
+                Map(m => m.APIEndpoint);
+                Map(m => m.NumOfOperands);
+                Map(m => m.OperandType);
+            }
+        }
+    }
+}
