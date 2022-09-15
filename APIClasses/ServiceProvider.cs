11d507d2ea9e6b1545238c6c313ec6c984caf54f
Stevie Hollard <20236092@student.curtin.edu.au>
2022-09-15 10:25:20 +0800

    Addition of API classes for the service provider

---
+++ APIClasses/ServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APIClasses
+{
+    public class ServiceProvider
+    {
+        public class TwoNums
+        {
+            public int NumOne;
+            public int NumTwo;
+        }
+
+        public class ThreeNums
+        {
+            public int NumOne;
+            public int NumTwo;
+            public int NumThree;
+        }
+    }
+}
